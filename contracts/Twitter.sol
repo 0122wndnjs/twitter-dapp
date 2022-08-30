f5742c855e61fd8e68542fdf0d41a242e853d294
Joowon Kim <0122wndnjs@gmail.com>
2022-08-30 14:42:35 +0900

    Functionality to add a tweet

--- contracts/Twitter.sol
+++ contracts/Twitter.sol
@@ -29,7 +29,7 @@ contract Twitter {
     mapping(uint256 => tweet) Tweets; // id to tweet struct
     mapping(address => user) Users; // address to a user struct
 
-    event tweetCreated (
+    event tweetCreated(
         address tweeter,
         uint256 id,
         string tweetText,
@@ -38,8 +38,31 @@ contract Twitter {
         uint256 timestamp
     );
 
-    event tweetDeleted (
-        uint256 id,
-        bool isDeleted
-    );
+    event tweetDeleted(uint256 id, bool isDeleted);
+
+    // Method to add a Tweet
+
+    function addTweet(string memory tweetText, string memory tweetImg)
+        public
+        payable
+    {
+        require(msg.value == (0.01 ether), "Please submit 0.01 MATIC");
+        tweet storage newTweet = Tweets[counter];
+        newTweet.tweetText = tweetText;
+        newTweet.tweetImg = tweetImg;
+        newTweet.tweeter = msg.sender;
+        newTweet.id = counter;
+        newTweet.isDeleted = false;
+        newTweet.timestamp = block.timestamp;
+        emit tweetCreated(
+            msg.sender,
+            counter,
+            tweetText,
+            tweetImg,
+            false,
+            block.timestamp
+        );
+        counter++;
+        payable(owner).transfer(msg.value);
+    }
 }
