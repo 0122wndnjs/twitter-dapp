610d998c73cb897f319fc8fa8e7f9ee09dd9c1b9
Joowon Kim <0122wndnjs@gmail.com>
2022-08-31 11:18:30 +0900

    working on App Components

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Settings from './pages/Settings';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='page'>
+        <div className='sideBar'>SideBar</div>
+        <div className='mainWindow'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/settings' element={<Settings />} />
+          </Routes>
+        </div>
+        <div className='rightBar'>RightBar</div>
+      </div>
+    </>
   );
 }
 
