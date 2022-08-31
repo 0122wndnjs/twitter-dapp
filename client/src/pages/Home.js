1bad4e846f200e7f31aa0ea19639f71b636a9477
Joowon Kim <0122wndnjs@gmail.com>
2022-08-31 14:32:00 +0900

    working on HomePage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,19 +1,46 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useRef } from "react";
 import "./Home.css";
+import { Avatar } from "@web3uikit/core";
+import { Image } from "@web3uikit/icons";
+import { defaultImgs } from "../defaultImgs";
 
 const Home = () => {
+  const inputFile = useRef(null);
+  const [selectedImage, setSelectedImage] = useState();
+  const [tweetText, setTweetText] = useState("");
+
+  const onImageClick = () => {
+    inputFile.current.click();
+  };
+
+  const changeHandler = (event) => {
+    const imgFile = event.target.files[0];
+    setSelectedImage(URL.createObjectURL(imgFile));
+  };
+
   return (
     <>
-      <Link to="/">
-        <div>Home</div>
-      </Link>
-      <Link to="/profile">
-        <div>Profile</div>
-      </Link>
-      <Link to="/settings">
-        <div>Settings</div>
-      </Link>
+      <div className="mainContent">
+        <div className="profileTweet">
+          <div className="tweetSection">
+            <Avatar isRounded image={defaultImgs[0]} theme="image" size={60} />
+            <textarea
+              name="TweetTxtArea"
+              placeholder="What's going on ?"
+              className="textArea"
+              onChange={(e) => setTweetText(e.target.value)}
+            ></textarea>
+          </div>
+          <div className="tweetSection">
+            <div className="imgDiv" onClick={onImageClick}>
+              <input type="file" ref={inputFile} onChange={changeHandler} style={{display:"none"}} />
+              { selectedImage ? <img src={selectedImage} width={150} /> : <Image fontSize={25} fill="#ffffff" /> }
+            </div>
+            <div className="tweet">Tweet</div>
+          </div>
+        </div>
+        {/* Feed section */}
+      </div>
     </>
   );
 };
