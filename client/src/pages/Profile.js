510ea41f2ea4469b42d1c08bb8a7ec9235e2d740
Joowon Kim <0122wndnjs@gmail.com>
2022-09-01 12:18:35 +0900

    working on Profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,19 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Profile.css";
+import { defaultImgs } from "../defaultImgs";
+import TweetInFeed from "../components/TweetInFeed";
 
 const Profile = () => {
   return (
     <>
-      <Link to="/">
-        <div>Home</div>
-      </Link>
-      <Link to="/profile">
-        <div>Profile</div>
-      </Link>
-      <Link to="/settings">
-        <div>Settings</div>
-      </Link>
+      <img className="profileBanner" src={defaultImgs[1]} />
+      <div className="pfpContainer">
+        <img className="profilePFP" src={defaultImgs[0]} />
+        <div className="profileName">Joowon Kim</div>
+        <div className="profileWallet">0x412351235123</div>
+        <Link to="./settings">
+          <div className="profileEdit">Edit Profile</div>
+        </Link>
+        <div className="profileBio">A middle class web3 developer</div>
+        <div className="profileTabs">
+          <div className="profileTab">Your Tweets</div>
+        </div>
+      </div>
+      <TweetInFeed profile={true}></TweetInFeed>
     </>
   );
 };
