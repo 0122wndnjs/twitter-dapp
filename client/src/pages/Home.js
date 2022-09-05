b93f3d2a343488c52e7e5385e1ac22cfc4afb1ec
Joowon Kim <0122wndnjs@gmail.com>
2022-09-05 11:59:23 +0900

    Setup Web3 Storage IPFS

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,30 @@
 import React, { useState, useRef } from "react";
 import "./Home.css";
-import { Avatar } from "@web3uikit/core";
+import { Avatar, Loading, useNotification } from "@web3uikit/core";
 import { Image } from "@web3uikit/icons";
 import { defaultImgs } from "../defaultImgs";
 import TweetInFeed from "../components/TweetInFeed";
+import { ethers } from "ethers";
+import Web3Modal from "web3modal";
+import { TwitterContractAddress, Web3StorageApi } from "../config";
+import TwitterAbi from "../abi/Twitter.json";
+import { Web3Storage } from "web3.storage";
 
 const Home = () => {
   const inputFile = useRef(null);
   const [selectedImage, setSelectedImage] = useState();
   const [tweetText, setTweetText] = useState("");
+  const userImage = JSON.parse(localStorage.getItem("userImage"));
+  const [selectedFile, setSelectedFile] = useState();
+  const [uploading, setUploading] = useState(false);
+  let ipfsUploadedUrl = "";
+  const notification = useNotification();
+
+  async function storeFile() {
+    const client = new Web3Storage({ token: Web3StorageApi });
+    const rootCid = await client.put(selectedFile);
+    ipfsUploadedUrl = `https://${rootCid}.ipfs.dweb.link/${selectedFile[0].name}`;
+  }
 
   const onImageClick = () => {
     inputFile.current.click();
@@ -17,6 +33,7 @@ const Home = () => {
   const changeHandler = (event) => {
     const imgFile = event.target.files[0];
     setSelectedImage(URL.createObjectURL(imgFile));
+    setSelectedFile(event.target.files);
   };
 
   return (
