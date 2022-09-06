408225006536e794c7f99c508bf02700156b9c6d
Joowon Kim <0122wndnjs@gmail.com>
2022-09-06 11:32:03 +0900

    function to update profile details

--- client/src/pages/Settings.js
+++ client/src/pages/Settings.js
@@ -1,13 +1,33 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import "./Settings.css";
-import { Input, Upload } from "@web3uikit/core";
+import { Input, Upload, Loading, useNotification } from "@web3uikit/core";
+import { ethers } from "ethers";
+import Web3Modal from "web3modal";
+import { TwitterContractAddress, Web3StorageApi } from "../config";
+import TwitterAbi from "../abi/Twitter.json";
+import { Web3Storage } from "web3.storage";
 
 const Settings = () => {
+  const notification = useNotification();
+  const userName = JSON.parse(localStorage.getItem("userName"));
+  const userBio = JSON.parse(localStorage.getItem("userBio"));
+  const userImage = JSON.parse(localStorage.getItem("userImage"));
+  const userBanner = JSON.parse(localStorage.getItem("userBanner"));
+
   const [profileFile, setProfileFile] = useState();
   const [bannerFile, setBannerFile] = useState();
-  const [name, setName] = useState();
-  const [bio, setBio] = useState();
+  const [name, setName] = useState(userName);
+  const [bio, setBio] = useState(userBio);
+  const [loading, setLoading] = useState(false);
+  let profileUploadedUrl = userImage;
+  let bannerUploadedUrl = userBanner;
+
+  async function storeFile(selectedFile) {
+    const client = new Web3Storage({ token: Web3StorageApi });
+    const rootCid = await client.put(selectedFile);
+    let ipfsUploadedUrl = `https://${rootCid}.ipfs.dweb.link/${selectedFile[0].name}`;
+    return ipfsUploadedUrl;
+  }
 
   const bannerHandler = (event) => {
     if (event != null) {
@@ -21,6 +41,57 @@ const Settings = () => {
     }
   };
 
+  useEffect(() => {}, [loading]);
+
+  async function updateProfile() {
+    setLoading(true);
+    if (profileFile != null) {
+      let newProfileUploadedUrl = await storeFile([profileFile]);
+      profileUploadedUrl = newProfileUploadedUrl;
+    }
+
+    if (bannerFile != null) {
+      let newBannerUploadedUrl = await storeFile([bannerFile]);
+      bannerUploadedUrl = newBannerUploadedUrl;
+    }
+
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(
+      TwitterContractAddress,
+      TwitterAbi.abi,
+      signer
+    );
+    const transaction = await contract.updateUser(
+      name,
+      bio,
+      profileUploadedUrl,
+      bannerUploadedUrl
+    );
+    await transaction.wait();
+
+    window.localStorage.setItem("userName", JSON.stringify(name));
+    window.localStorage.setItem("userBio", JSON.stringify(bio));
+    window.localStorage.setItem(
+      "userImage",
+      JSON.stringify(profileUploadedUrl)
+    );
+    window.localStorage.setItem(
+      "userBanner",
+      JSON.stringify(bannerUploadedUrl)
+    );
+
+    notification({
+      type: "success",
+      title: "Profile Updated Successfully",
+      position: "topR",
+    });
+
+    setLoading(false);
+  }
+
   return (
     <>
       <div className="settingsPage">
@@ -30,6 +101,7 @@ const Settings = () => {
           width="100%"
           labelBgColor="#141d26"
           onChange={(e) => setName(e.target.value)}
+          value={userName}
         />
         <Input
           label="Bio"
@@ -37,12 +109,21 @@ const Settings = () => {
           width="100%"
           labelBgColor="#141d26"
           onChange={(e) => setBio(e.target.value)}
+          value={userBio}
         />
         <div className="pfp">Change Profile Image</div>
         <Upload onChange={profileHandler} />
         <div className="pfp">Change Banner Image</div>
         <Upload onChange={bannerHandler} />
-        <div className="save">Save</div>
+        {loading ? (
+          <div className="save">
+            <Loading />
+          </div>
+        ) : (
+          <div className="save" onClick={updateProfile}>
+            Save
+          </div>
+        )}
       </div>
     </>
   );
